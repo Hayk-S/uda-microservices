10ff93ead8183ff30a214be90f6cb69a020bef5b
Hayk-S <hayksardaryan555@gmail.com>
2021-04-08 20:48:27 -0400

    changing frontend url to reversproxy url in aws

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a6b7c7eb8393940ee9f853d2e5876e6a-1304640808.ca-central-1.elb.amazonaws.com'
 };
 
 /*
